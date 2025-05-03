c6b9208db628c13c68285c9274ffbac5278a80db
vinod-kumar1 <vinu22149@gmail.com>
2025-05-03 23:30:08 +0530

    added js and removed cache file

---
+++ app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  React.createElement("div", { id: "test" }, "Hello Vinod! tester")
+);
---
+++ dist/namaste-react.7c0ccee6.js
@@ -0,0 +1,5 @@
+ReactDOM.createRoot(document.getElementById("root")).render(React.createElement("div", {
+    id: "test"
+}, "Hello Vinod"));
+
+//# sourceMappingURL=namaste-react.7c0ccee6.js.map
